75a1037fa1712a5ae529017dc5bdbb02b6c47a86
valebru <valebrshk@gmail.com>
2019-06-05 16:18:21 +0200

    first commit - src web site

---
+++ src-dashboard/src/app/realtime/realtime.controller.js
@@ -0,0 +1,105 @@
+(function () {
+    'use strict';
+
+    angular
+      .module('angularProjects')
+      .controller('RealTimeController', RealTimeController);
+
+    /** @ngInject */
+    function RealTimeController($http) {
+      var vm = this;
+
+       $http({
+         method: 'GET',
+         url: 'http://flow-monitor:5000/realtime'
+       }).then(function successCallback(response) {
+         vm.data[0].values = [];
+         for (var i = 0; i < response.data["samples"].length; i++) {
+           vm.data[0].values.push({
+             label: response.data["samples"][i]["ip"],
+             value: response.data["samples"][i]["pps"]
+           });
+         }
+       });
+
+
+
+
+      vm.options = {
+        chart: {
+          type: 'discreteBarChart',
+          height: 450,
+          width: 1000,
+          margin: {
+            top: 20,
+            right: 20,
+            bottom: 50,
+            left: 70
+          },
+          x: function (d) {
+            return d.label;
+          },
+          y: function (d) {
+            return d.value;
+          },
+          showValues: true,
+
+          valueFormat: function (d) {
+            return d3.format('d')(d);
+          },
+          duration: 500,
+          xAxis: {
+            axisLabel: 'flows',
+            rotateLabels: -30
+          },
+          yAxis: {
+            axisLabel: 'pps',
+            tickFormat: function (d) {
+              return d3.format('d')(d)
+            },
+          }
+        }
+      };
+
+      vm.data = [
+        {
+          key: "",
+          values: []
+        }
+      ]
+
+
+      setInterval(function () {
+
+        $http({
+          method: 'GET',
+          url: 'http://flow-monitor:5000/realtime'
+        }).then(function successCallback(response) {
+          if (response.data["samples"].length === 0) {
+            return
+          }
+          ;
+
+          // this callback will be called asynchronously
+          // when the response is available
+          console.log("GET OK");
+          vm.data[0].values = [];
+
+          for (var i = 0; i < response.data["samples"].length; i++) {
+            vm.data[0].values.push({
+              label: response.data["samples"][i]["ip"],
+              value: response.data["samples"][i]["pps"]
+            });
+          }
+
+        }, function errorCallback(response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+          console.log("GET ERROR");
+        });
+
+      }, 4000);
+
+      return vm;
+    }
+  })();
